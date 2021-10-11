cfac8c693c5441e08d54c4f21a349f1cf69b629f
zy <1018390434@qq.com>
2021-10-11 23:46:34 +0800

    asdf

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import GlobalComponents from './components/global/index.js'
 // import Vconsole from "vconsole";
 // var vConsole = new Vconsole();
+// import "lodash";
+import { add } from "lodash";
+// const add = require('lodash/add')
+console.log('qwe add', add(2, 9));
 
 Vue.use(GlobalComponents)
 var version = Number(Vue.version.split('.')[0])
